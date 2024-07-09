33f0f953182a476169ba7869f7c03d88b9bc09c1
Breshnahamid20 <breshna2004@gmail.com>
2024-07-09 17:32:38 +0430

    solve linters error

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ document.getElementById('convert-btn').addEventListener('click', () => {
     output.innerHTML = 'Please enter a number less than or equal to 3999';
   } else {
     let romanNumeral = '';
-    let romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    let arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    const arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     for (let i = 0; i < romanNumerals.length; i += 1) {
       while (number >= arabicNumbers[i]) {
         romanNumeral += romanNumerals[i];
