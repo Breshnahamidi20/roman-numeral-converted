ebfde7bb75c2dad0f4bc77a347f256e8e6d3c8d9
Breshnahamid20 <breshna2004@gmail.com>
2024-07-09 15:07:56 +0430

    solve linters error

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ document.getElementById('convert-btn').addEventListener('click', () => {
   const output = document.getElementById('output');
   if (Number.isNaN || number === '') {
     output.innerHTML = 'Please enter a valid number';
-  } else if (isNaN(number) || number < 1) {
+  } else if (number.isNaN || number < 1) {
     output.innerHTML = 'Please enter a number greater than or equal to 1';
   } else if (number >= 4000) {
     output.innerHTML = 'Please enter a number less than or equal to 3999';
   } else {
-    const romanNumeral = '';
-    const romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    const arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romanNumeral = '';
+    let romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     for (const i = 0; i < romanNumerals.length; i += 1) {
       while (number >= arabicNumbers[i]) {
        romanNumeral += romanNumerals[i];
-       number -= arabicNumbers[i];
+        number -= arabicNumbers[i];
       }
     }
     output.innerHTML = romanNumeral;
